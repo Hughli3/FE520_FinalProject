ad2d341918f4d438f832a2a44cb7a50410abfa92
hughli <hughli@live.com>
2020-04-10 00:29:23 -0400

    create new file

--- app.py
+++ app.py
@@ -6,11 +6,13 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        pass
-    else:
-        return render_template("index.html")
-    
+    try:
+        if request.method == 'POST':
+            pass
+        else:
+            return render_template("index.html")
+    except Exception as e:
+        print(e) 
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
