d8e7b6df5191d34ea37b182c3334702537de6d0a
hughli <hughli@live.com>
2020-04-12 16:04:52 -0400

    modified search page

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
 from flask import Flask, render_template,url_for,request
 from datetime import datetime
 
+## =================== Search function ===================
+
+
+
+## =================== Get data function ===================
+
+##
 app = Flask(__name__)
 
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
     try:
-        if request.method == 'POST':
-            pass
-        else:
-            return render_template("index.html")
+        # if request.method == "GET":
+        #     search = request.form["search-form"]
+        #     print(search)
+        #     # Use request to get stock code
+
+        #     # use code get the stock data
+
+        #     # rerender the page by passing the stock data into view
+        #     # return render_template("view.html", stockData = data)
+        #     return render_template("index.html")
+        # else:
+        return render_template("index.html")
+
     except Exception as e:
         print(e)
 
