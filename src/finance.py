b62154c41d180280721766f53668067b801e618b
Zaning <61484146+zaning424@users.noreply.github.com>
2020-05-02 21:21:57 -0400

    implement finance

--- src/finance.py
+++ src/finance.py
@@ -1,16 +1,33 @@
-## =================== import packages ===================
+import pandas as pd
+import numpy as np
+import pandas_datareader.data as web
 
 
-## =================== Main functions ===================
-def RSI(data:list) ->list:
-    """
-    
-    """
+def rsi(data, time):
+    delta = data.diff().dropna()
+    u = delta * 0
+    d = u.copy()
+    u[delta > 0] = delta[delta > 0]
+    d[delta < 0] = - delta[delta < 0]
+    u_ave = u.ewm(com=time-1, min_periods=time).mean()
+    d_ave = d.ewm(com=time-1, min_periods=time).mean()
+    rs = abs(u_ave/d_ave)
+    rsi = 100 - (100/(1+rs))
+    return rsi
     pass
 
 
-def KDG(data:list) ->list:
+def sma(data, window):
+    weights = np.repeat(1.0, window)/ window
+    smas = np.convolve(data, weights, 'valid')
+    return smas
     pass
 
 
-
+def macd(data):
+    exp1 = data.ewm(span=12, adjust=False).mean()
+    exp2 = data.ewm(span=26, adjust=False).mean()
+    macd = exp1 - exp2
+    exp3 = macd.ewm(span=9, adjust=False).mean()
+    return macd
+    pass
