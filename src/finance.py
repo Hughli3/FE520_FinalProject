15000100cfbcab913d9a4c3035ea32957cc613a2
Zaning <61484146+zaning424@users.noreply.github.com>
2020-05-07 00:20:29 -0400

    Update finance.py

--- src/finance.py
+++ src/finance.py
@@ -2,83 +2,76 @@ import pandas as pd
 import numpy as np
 import pandas_datareader.data as web
 import datetime
-start = datetime.datetime(2016,1,1)
-end = datetime.date.today()
 
 
-# stock_name, stok_code:str, start_date:[int], end_date:[int]
-end_date = datetime.date.today()
-# three years ago
-start_date = end_date - datetime.timedelta(days=1095)
+class Indicator():
+    def __init__(self, data):
+        self.data = data
 
-data = web.DataReader("AAPL", "yahoo", start_date, end_date)
+    def rsi(self, time=14):
+        # set default to 14 days
+        adj = self.data['Adj Close']
+        delta = adj.diff().dropna()
+        u = delta * 0
+        d = u.copy()
+        u[delta > 0] = delta[delta > 0]
+        d[delta < 0] = - delta[delta < 0]
+        u_ave = u.ewm(com=time-1, min_periods=time).mean()
+        d_ave = d.ewm(com=time-1, min_periods=time).mean()
+        rs = abs(u_ave/d_ave)
+        rsi = 100 - (100/(1+rs))
+        return rsi
 
-data = pd.DataFrame(data)
+    # calculating SMA, column = type(ex. AdjCLose, Close, High)
+    # column default as 5 = Adjusted Close
+    # period = time window of SMA (e.g. 5days sma = 5)
+    def sma(self, column=5, period=5):
+        sma = self.data.iloc[:, column].rolling(window=period).mean()
+        return sma
+
+    # Calculating MACD
+    def macd(self):
+        adj = self.data['Adj Close']
+        # find 12-period EMA
+        exp1 = adj.ewm(span=12, adjust=False).mean()
+        # find 26-period EMA
+        exp2 = adj.ewm(span=26, adjust=False).mean()
+        macd = exp1 - exp2
+        return macd
+
+    # find macd_signal value
+    def macd_signal(self):
+        adj = self.data['Adj Close']
+        # Calculating DEA indicator with period of 9
+        exp1 = adj.ewm(span=12, adjust=False).mean()
+        exp2 = adj.ewm(span=26, adjust=False).mean()
+        macd = exp1 - exp2
+        exp3 = macd.ewm(span=9, adjust=False).mean()
+        return exp3
+
+    def add_indicator(self):
+        # add RSI indicator of each day to data
+        self.data['RSI'] = self.rsi()
+
+        self.data['SMA_10'] = self.sma(5,10)
+        self.data['SMA_20'] = self.sma(5, 20)
+        self.data['SMA_30'] = self.sma(5, 30)
+        # add MACD indicator to data
+        # this column indicate the MACD line in macd chart
+        self.data['MACD'] = self.macd()
+
+        # add MACD_Signal indicator to data
+        # this column indicate the signal line in macd chart
+        self.data['MACD_signal'] = self.macd_signal()
+
+        # add MACD_diff indicator to data
+        # this column indicate the histogram in macd chart
+        self.data['MACD_diff'] = self.data['MACD'] - self.data['MACD_signal']
+        rsi_today = self.data.iloc[-1,6]
+        monthly_sma = self.data.iloc[-1,9]
+        macd_today = self.data.iloc[-1,10]
+        return self.data, rsi_today, monthly_sma, macd_today
 
 
-def rsi(data, time=14):
-    # set default to 14 days
-    delta = data.diff().dropna()
-    u = delta * 0
-    d = u.copy()
-    u[delta > 0] = delta[delta > 0]
-    d[delta < 0] = - delta[delta < 0]
-    u_ave = u.ewm(com=time-1, min_periods=time).mean()
-    d_ave = d.ewm(com=time-1, min_periods=time).mean()
-    rs = abs(u_ave/d_ave)
-    rsi = 100 - (100/(1+rs))
-    return rsi
 
-
-# Calculating MACD
-def macd(data):
-    # find 12-period EMA
-    exp1 = data.ewm(span=12, adjust=False).mean()
-    # find 26-period EMA
-    exp2 = data.ewm(span=26, adjust=False).mean()
-    macd = exp1 - exp2
-    return macd
-
-
-# find macd_signal value
-def macd_signal(data):
-    # Calculating DEA indicator with period of 9
-    exp1 = data.ewm(span=12, adjust=False).mean()
-    exp2 = data.ewm(span=26, adjust=False).mean()
-    macd = exp1 - exp2
-    exp3 = macd.ewm(span=9, adjust=False).mean()
-    return exp3
-
-
-# add RSI indicator of each day to 7th column
-data['RSI'] = rsi(data['Adj Close'])
-
-# change value of window in .rolling to change period of SMA
-# change value of data.iloc[:,n] to change column used to calculate
-# data.iloc[:,3] is SMA using close price
-# data.iloc[:,5] is adjusted close; [:,0] is high
-
-# add three day moving average of adjust close to 8th column
-data['SMA_3'] = data.iloc[:,5].rolling(window=3).mean()
-
-# add five day moving average to 9th column
-data['SMA_5'] = data.iloc[:,5].rolling(window=5).mean()
-
-# add ten day moving average to 10th column
-data['SMA_10'] = data.iloc[:,5].rolling(window=10).mean()
-
-# add MACD indicator to 11th column
-# this column indicate the MACD line in macd chart
-data['MACD'] = macd(data['Adj Close'])
-
-# add MACD_Signal indicator to 12th column for charting
-# this column indicate the signal line in macd chart
-data['MACD_signal'] = macd_signal(data['Adj Close'])
-
-# add MACD_diff indicator to 13th column for charting
-# this column indicate the histogram in macd chart
-data['MACD_diff'] = data['MACD'] - data['MACD_signal']
-
-print(data.head)
-print(data.columns)
 
